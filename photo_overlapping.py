e549bc4d22b497f5effa90eb618ce5b9770335bb
Rakshita Maheshwari <rakshitamaheshwari99@gmail.com>
2019-07-09 23:02:45 +0530

    overlap two photos

--- photo_overlapping.py
+++ photo_overlapping.py
@@ -1,47 +1,15 @@
-
-# coding: utf-8
-
-# In[3]:
-
-
 import cv2
-
-
-# In[4]:
-
-
 x=cv2.VideoCapture(0)
-
-
-# In[ ]:
-
-
 ret,pic=x.read()
 ret1,pic1=x.read()
-
-
-# In[5]:
-
 
 pic = cv2.imread("pic.png")
 pic1=cv2.imread("pic1.png")
 
-
-# In[ ]:
-
-
 cv2.imwrite("pic.png",pic)
 cv2.imwrite("pic1.png",pic1)
 
-
-# In[9]:
-
-
 pic1[0:100,0:100]=pic[200:300,300:400]
-
-
-# In[10]:
-
 
 cv2.imshow("hi",pic1)
 cv2.waitKey()
