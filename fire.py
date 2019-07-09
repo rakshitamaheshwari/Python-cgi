136c0c9663706b66ee14a7b88ba608916a46574d
Rakshita Maheshwari <rakshitamaheshwari99@gmail.com>
2019-07-09 22:56:22 +0530

    check firewall status

--- fire.py
+++ fire.py
@@ -1,10 +1,7 @@
 import subprocess as sp
-#y="systemctl status firewalld"
 x=sp.getoutput("ansible localhost -m command -a 'systemctl status firewalld' ")
 if "inactive" in  x:
-	print("noactive")
+	print("not active")
 else:
-	print("yes") 
-#print(x)
-#<?xml version="1.0"?>
-#<?xml-stylesheet type="text/xsl" href="configuration.xsl"?>
+	print("active") 
+
