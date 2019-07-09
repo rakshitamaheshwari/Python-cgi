86e8a8e271905e7b803a9b7f21335e13e04aff04
Rakshita Maheshwari <rakshitamaheshwari99@gmail.com>
2019-07-09 22:58:04 +0530

    Delete mail.py

--- mail.py
+++
@@ -1,7 +0,0 @@
-import cv2
-import os
-cap=cv2.VideoCapture(0)
-ret,photo = cap.read()
-cv2.imwrite("/root/Desktop/menu.png", photo)
-cap.release()
-os.system("ansible-playbook /root/Desktop/ansiblecode/mail.yml")
