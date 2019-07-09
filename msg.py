b604b4f31bcae3a2f62e43fe3288839ee2ba7693
Rakshita Maheshwari <rakshitamaheshwari99@gmail.com>
2019-07-09 23:00:43 +0530

    Update msg.py

--- msg.py
+++ msg.py
@@ -1,7 +1,7 @@
 
 from twilio.rest import Client
-account_sid= "AC5186e3d0233838a2236dd8a081a96935"
-auth_token= "079a26d27271e7dbd81bc6283e8c1a9c"
+account_sid= "**************************"
+auth_token= "***************************"
 client=Client(account_sid,auth_token)
-message=client.messages.create(from_='+13082170920',body='hello from twilio',to='+91 8209046716')
+message=client.messages.create(from_='twilio_number',body='hello from twilio',to='your_number')
 print(message.sid)
